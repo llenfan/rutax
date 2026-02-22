fba75d0cf31b3b1e1b85325384caa7a8df858789
llenfan <llenfan@example.com>
2026-02-22 17:22:45 -0300

    Fix server.js console.log syntax

--- server.js
+++ server.js
@@ -44,5 +44,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(PORT, () => {
-  console.log(\`Server running at http://localhost:\${PORT}\`);
+  console.log('Server running at http://localhost:' + PORT);
 });
